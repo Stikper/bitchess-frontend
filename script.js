735ba8646d97c67fa57c64b20601dfffd55d02fb
Kirill Cherepanov <kirill2005910@gmail.com>
2022-02-06 17:45:05 +0300

    Create board

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 let blur_area = null;
+let board = null;
 
 function preSetup(){
     window.addEventListener('resize', function (){styleCheck()});
     blur_area = document.getElementById('blur_area');
+    board = document.getElementById("board");
     styleCheck();
+    createBoard();
 }
 
 function styleCheck() {
@@ -15,5 +18,20 @@ function styleCheck() {
     } else {
         blur_area.style.setProperty('--main_border_radius', '1.59vw');
         blur_area.style.setProperty('--clock_margin', '2.85vw');
+    }
+}
+
+function createBoard() {
+    let color_flag = false;
+    for (let i = 8; i>0; i--) {
+        for(let j = 1; j < 8 + 1; j++) {
+            let square = document.createElement('div');
+            if (color_flag) square.className = 'square black_square';
+            else square.className = 'square white_square';
+            color_flag = !color_flag;
+            square.id = String.fromCharCode(64 + j) + i;
+            board.appendChild(square);
+        }
+        color_flag = !color_flag;
     }
 }
